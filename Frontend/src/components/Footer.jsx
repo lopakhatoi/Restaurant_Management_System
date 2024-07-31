6b010b7cba85172238fe4b3801d211801dbc66a6
lopakhatoi <lopakhatoi@gmail.com>
2024-07-30 22:50:39 -0700

    uploaded first

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">LOPA</div>
+          <div className="right">
+            <p>New AG Colony, BBSR</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By LOPA</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By LOPA.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
